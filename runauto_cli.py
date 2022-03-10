68e73f85fae63ede5cf6e40b6209324c38e6c0a2
David Hoogerheide <david.hoogerheide@nist.gov>
2022-03-10 11:22:38 -0500

    generalize command line run script

--- runauto_cli.py
+++ runauto_cli.py
@@ -18,37 +18,35 @@ plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['font.size'] = 16
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--eta', type=float)
-parser.add_argument('--alpha', type=float)
-parser.add_argument('--npoints', type=int)
-parser.add_argument('--nrepeats', type=int)
-parser.add_argument('--maxtime', type=float)
-parser.add_argument('--penalty', type=float)
-parser.add_argument('--timepenalty', type=float)
-parser.add_argument('--burn', type=int)
-parser.add_argument('--steps', type=int)
+parser.add_argument('model', type=str)
+parser.add_argument('--pars', type=str)
+parser.add_argument('--name', type=str)
+parser.add_argument('--sel', type=int, nargs='+')
+parser.add_argument('--meas_bkg', type=float, nargs='+')
+parser.add_argument('--eta', type=float, default=0.8)
+parser.add_argument('--alpha', type=float, default=0.001)
+parser.add_argument('--npoints', type=int, default=1)
+parser.add_argument('--nrepeats', type=int, default=1)
+parser.add_argument('--maxtime', type=float, default=21.6e3)
+parser.add_argument('--penalty', type=float, default=1.0)
+parser.add_argument('--timepenalty', type=float, default=0.0)
+parser.add_argument('--burn', type=int, default=1000)
+parser.add_argument('--steps', type=int, default=500)
+parser.add_argument('--pop', type=int, default=8)
 parser.add_argument('--resume', type=str)
-parser.add_argument('--instrument', type=str)
+parser.add_argument('--instrument', type=str, default='MAGIK')
+parser.add_argument('--oversampling', type=int, default=11)
 args = parser.parse_args()
 
 # define fit options dictionary
-fit_options = {'burn': 1000, 'pop': 8, 'steps': 500, 'init': 'lhs', 'alpha': 0.001}
+fit_keys = ['burn', 'pop', 'steps', 'init', 'alpha']
+fit_options = dict([(k, getattr(args, k)) for k in fit_keys])
+#fit_options = {'burn': 1000, 'pop': 8, 'steps': 500, 'init': 'lhs', 'alpha': 0.001}
 
 if __name__ == '__main__':
 
     # plot
     movieyn = True
-
-    #%%
-    eta = 0.8 if args.eta is None else args.eta
-    npoints = 1 if args.npoints is None else args.npoints
-    maxtime = 21.6e3 if args.maxtime is None else args.maxtime
-    nrepeats = 1 if args.nrepeats is None else args.nrepeats
-    penalty = 1.0 if args.penalty is None else args.penalty
-    switch_time_penalty = 0.0 if args.timepenalty is None else args.timepenalty
-    fit_options['alpha'] = 0.001 if args.alpha is None else args.alpha
-    fit_options['burn'] = 1000 if args.burn is None else args.burn
-    fit_options['steps'] = 500 if args.steps is None else args.steps
 
     if args.resume is None:
 
@@ -59,35 +57,36 @@ if __name__ == '__main__':
         else:
             raise ValueError('instrument must be MAGIK or CANDOR')
 
-        fprefix = '%s_eta%0.2f_npoints%i_repeats%i' % (instr.name, eta, npoints, nrepeats)
+        fprefix = '%s_eta%0.2f_npoints%i_repeats%i' % (instr.name, args.eta, args.npoints, args.nrepeats)
+        fsuffix = '' if args.name is None else args.name
 
         # define file name and create results directory based on timestamp
         fn = copy.copy(datetime.datetime.now().strftime('%Y%m%dT%H%M%S'))
-        pathname = fprefix + '_' + fn
+        pathname = fprefix + '_' + fn + '_' + fsuffix
         os.mkdir(pathname)
         #fn = './' + fn + fsuffix + '/' + fn
 
         #%%
         # define calculation probe from model file (runs calculation for all models in model file)
-        modelfile = 'ssblm.py'
-        bestpars = 'ssblm.par'
-
+        modelfile = args.model
         model = load_model(modelfile)
-        bestp = np.array([float(line.split(' ')[-1]) for line in open(bestpars, 'r').readlines()])
+
+        bestpars = args.pars
+        bestp = np.array([float(line.split(' ')[-1]) for line in open(bestpars, 'r').readlines()]) if bestpars is not None else None
 
         # calculation vector
         measQ = np.linspace(0.008, 0.25, 201)
 
-        # parameters to use for marginalization
-        sel = np.array([10, 11, 12, 13, 14])
+        # measurement background
+        meas_bkg = np.full(len(model.models), 1e-5) if args.meas_bkg is not None else args.meas_bkg
 
         for kk in range(nrepeats):
-            exp = SimReflExperiment(model, measQ, instrument=instr, eta=eta, fit_options=fit_options, oversampling=11, bestpars=bestp, select_pars=sel, meas_bkg=[3e-6, 3e-5], switch_penalty=penalty)
-            exp.switch_time_penalty = switch_time_penalty # takes time to switch models
+            exp = SimReflExperiment(model, measQ, instrument=args.instr, eta=args.eta, fit_options=fit_options, oversampling=11, bestpars=bestp, select_pars=args.sel, meas_bkg=meas_bkg, switch_penalty=args.penalty)
+            exp.switch_time_penalty = args.switch_time_penalty # takes time to switch models
             exp.add_initial_step()
             total_t = 0.0
             k = 0
-            while total_t < maxtime:
+            while total_t < args.maxtime:
                 total_t += exp.steps[-1].meastime() + exp.steps[-1].movetime()
                 print('Rep: %i, Step: %i, Total time so far: %0.1f' % (kk, k, total_t))
                 exp.fit_step()
@@ -112,7 +111,7 @@ if __name__ == '__main__':
 
         total_t = sum([step.meastime() + step.movetime() for step in exp.steps[:-1]])
         k = len(exp.steps) - 1
-        while total_t < maxtime:
+        while total_t < args.maxtime:
             total_t += exp.steps[-1].meastime() + exp.steps[-1].movetime()
             print('Resumed, Step: %i, Total time so far: %0.1f' % (k, total_t))
             exp.fit_step()
